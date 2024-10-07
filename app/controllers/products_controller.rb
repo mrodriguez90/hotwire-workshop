0b194b960a2baffc07f5abe6e20b1ae640f98541
Mauricio Rodríguez <35469032+mrodriguez90@users.noreply.github.com>
2024-10-07 14:54:45 -0300

    Add filters (#35)
    * Add filters

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,15 +3,22 @@
 class ProductsController < ApplicationController
   def index
     @products = policy_scope(Product).includes(picture_attachment: :blob)
-    if params[:q].present?
-      @products = @products.where('name ILIKE ? OR description ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%")
-    end
-    @pagy, @products = pagy(@products, items: 1)
+    @products = filter_by_query(@products)
+    @products = filter_by_status(@products)
+    @pagy, @products = pagy(@products, items: 2)
   end
 
   private
 
-  def product_params
-    params
+  def filter_by_query(products)
+    return products if params[:q].blank?
+
+    products.where('name ILIKE ? OR description ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%")
+  end
+
+  def filter_by_status(products)
+    return products if params[:status].blank?
+
+    products.where(status: params[:status])
   end
 end
