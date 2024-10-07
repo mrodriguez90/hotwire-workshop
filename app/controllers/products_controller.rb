08e547d4f2fa20f822f2d7cd0374026ce326e8a2
Mauricio Rodríguez <35469032+mrodriguez90@users.noreply.github.com>
2024-10-07 10:29:05 -0300

    add products search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,16 @@
 
 class ProductsController < ApplicationController
   def index
-    @pagy, @products = pagy(policy_scope(Product).includes(picture_attachment: :blob), items: 1)
+    @products = policy_scope(Product).includes(picture_attachment: :blob)
+    if params[:q].present?
+      @products = @products.where('name ILIKE ? OR description ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%")
+    end
+    @pagy, @products = pagy(@products, items: 1)
+  end
+
+  private
+
+  def product_params
+    params
   end
 end
