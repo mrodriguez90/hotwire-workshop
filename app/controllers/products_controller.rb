8d8d65f24df239f15fcfa34c90a5cd031b0dbf25
Mauricio Rodríguez <35469032+mrodriguez90@users.noreply.github.com>
2024-10-23 13:57:12 -0300

    add products search by name or description (#34)
    * add products search and filters

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,23 @@
 
 class ProductsController < ApplicationController
   def index
-    @pagy, @products = pagy(policy_scope(Product).includes(picture_attachment: :blob), items: 1)
+    @products = policy_scope(Product).includes(picture_attachment: :blob)
+    @products = filter_by_query(@products)
+    @products = filter_by_status(@products)
+    @pagy, @products = pagy(@products, items: 2)
+  end
+
+  private
+
+  def filter_by_query(products)
+    return products if params[:q].blank?
+
+    products.where('name ILIKE ? OR description ILIKE ?', "%#{params[:q]}%", "%#{params[:q]}%")
+  end
+
+  def filter_by_status(products)
+    return products if params[:status].blank?
+
+    products.where(status: params[:status])
   end
 end
